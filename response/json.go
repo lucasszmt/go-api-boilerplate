1674068ccc44d228d5abae9e6effd6a85affc2c6
Lucas Szeremeta <lucasszmt@gmail.com>
2023-04-19 21:57:37 -0300

    fix: added unit tests

--- response/json.go
+++ response/json.go
@@ -9,8 +9,11 @@ import (
 func JSON(w http.ResponseWriter, statusCode int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	if err := json.NewEncoder(w).Encode(data); err != nil {
+
+	js, err := json.Marshal(data)
+	if err != nil {
 		return fmt.Errorf("json: %w", err)
 	}
+	w.Write(js)
 	return nil
 }
